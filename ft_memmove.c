c453912decc83faeb4c45fd99a77ba35268d52df
Elena Lavricheva <elavrich@c1r1s7.42prague.com>
2024-09-21 21:27:41 +0200

    too many lines f u

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:00:09 by elavrich          #+#    #+#             */
-/*   Updated: 2024/09/18 16:15:52 by elavrich         ###   ########.fr       */
+/*   Updated: 2024/09/21 21:27:27 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,25 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	char		*char_ptr;
-	const char	*char_ptr2;
+	size_t	i;
 
 	i = 0;
 	if (n == 0 || dest == src)
 		return (dest);
-	char_ptr = (char *)dest;
-	char_ptr2 = (const char *)src;
-	if (char_ptr > char_ptr2)
+	if (dest > src)
 	{
 		i = n;
 		while (i > 0)
 		{
 			i--;
-			char_ptr[i] = char_ptr2[i];
+			*(char *)(dest + i) = *(char *)(src + i);
 		}
 	}
 	else
 	{
 		while (i < n)
 		{
-			char_ptr[i] = char_ptr2[i];
+			*(char *)(dest + i) = *(char *)(src + i);
 			i++;
 		}
 	}
